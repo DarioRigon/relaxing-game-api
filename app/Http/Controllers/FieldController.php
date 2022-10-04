f57d38e9e9bfd540265e22ea16074ca5ed0caeaa
Dario Rigon <mangasprai@icloud.com>
2022-10-05 00:44:38 +0200

    fixed fields

--- app/Http/Controllers/FieldController.php
+++ app/Http/Controllers/FieldController.php
@@ -16,7 +16,7 @@ class FieldController extends Controller
      */
     public function index()
     {
-        return auth()->user()->fields->with('item');
+        return auth()->user()->fields;
     }
 
     /**
